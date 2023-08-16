8e2c1f9fd22b9d21b0b81667be90d4dfe5f90602
ValuPC <valentinabalo9@gmail.com>
2023-08-16 08:54:05 -0300

    configurado inicio de sesion con hash

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import UserModel from "../DAO/models/user.model.js";
+import {createHash, isValidPassword} from '../utils.js'
 
 const router = Router()
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body
-    const user = await UserModel.findOne({ email, password })
+    const user = await UserModel.findOne({ email })
     if(!user) {
         return res.redirect('/register')}
+
+        if(!isValidPassword(user, password)){
+            console.log('contraseña invalida')
+            return res.redirect('/register')
+        }
 
     req.session.user = user
     return res.redirect('/home.handlebars')
@@ -15,6 +21,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', async (req, res) => {
     const user = req.body
+    user.password =createHash(user.password)
     await UserModel.create(user)
 
     return res.redirect('/')
--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,14 @@ import {fileURLToPath} from 'url'
 import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+import bcrypt from 'bcrypt'
+
+export const createHash = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+}
+
+export const isValidPassword= (user,password)=>{
+    return bcrypt.compareSync(password, user.password)
+}
 
 export default __dirname
